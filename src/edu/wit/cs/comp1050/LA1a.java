1e37b01ef7d06a80353cd3d25bb6d49ecb0dc0f1
Nate Derbinsky <nate.derbinsky@gmail.com>
2017-01-15 06:05:50 -0500

    initial import

---
+++ src/edu/wit/cs/comp1050/LA1a.java
@@ -0,0 +1,51 @@
+package edu.wit.cs.comp1050;
+
+public class LA1a {
+	
+	/**
+	 * Returns an array with counts for each
+	 * letter from a-z (case-insensitive),
+	 * ignoring all other characters:
+	 * 
+	 * [0]: number of a/A's
+	 * [1]: number of b/B's
+	 * ...
+	 * [25]: number of z/Z's
+	 * 
+	 * @param word input word
+	 * @return count of case-insensitive letters
+	 */
+	public static int[] countLetters(String word) {
+		int[] counts = new int[26];
+		
+		// write your code here
+		
+		return counts;
+	}
+	
+	/**
+	 * Compares two arrays and
+	 * returns true if they have
+	 * the same contents
+	 * 
+	 * @param c1 array 1
+	 * @param c2 array 2
+	 * @return true if c1 and c2 have the same contents
+	 */
+	public static boolean sameCounts(int[] c1, int[] c2) {
+		return false; // write your code here
+	}
+
+	/**
+	 * Inputs two phrases and outputs
+	 * if they are anagrams (ignoring
+	 * case and any non-letter
+	 * characters)
+	 * 
+	 * @param args command-line arguments, ignored
+	 */
+	public static void main(String[] args) {
+		// write your code here
+	}
+
+}
